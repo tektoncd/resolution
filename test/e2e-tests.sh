7c227340a881e0e788798a864b7cf96e145ee4f4
Scott <sbws@google.com>
2022-04-06 17:39:40 +0000

    Deploy a test registry and bundle before e2e tests
    Prior to this commit the pipeline_bundles_test deployed its own test
    registry and a bundle. This took a long time and really slowed down the
    test.
    
    This commit attempts to create the registry and test bundle before
    executing tests, hopefully speeding things up and allowing all tests to
    share the same bundleRef.

--- test/e2e-tests.sh
+++ test/e2e-tests.sh
@@ -18,7 +18,7 @@ source $(git rev-parse --show-toplevel)/vendor/github.com/tektoncd/plumbing/scri
 
 initialize $@
 
-local failed=0
+failed=0
 
 header "Deploying Tekton Pipelines"
 git clone https://github.com/tektoncd/pipeline
@@ -45,6 +45,46 @@ ko apply -f ./docs/resolver-template/config
 kubectl patch -n tekton-pipelines configmap feature-flags -p '{"data":{"enable-api-fields":"alpha"}}'
 
 wait_until_pods_running "tekton-remote-resolution" || fail_test "Tekton Resolution did not come up"
+
+# Deploy a test registry with an example bundle that can be used across
+# multiple e2e tests.
+header "Deploying OCI Registry and a Test Bundle"
+kubectl create namespace tekton-resolution-example-registry
+kubectl apply -n tekton-resolution-example-registry -f ./test/bundle_registry/registry-deployment.yaml
+kubectl apply -n tekton-resolution-example-registry -f ./test/bundle_registry/registry-service.yaml
+
+wait_until_pods_running "tekton-resolution-example-registry" || fail_test "Test registry did not come up"
+
+TEST_REGISTRY_CLUSTER_IP=$(kubectl get service registry -n tekton-resolution-example-registry -o jsonpath="{$.spec.clusterIP}")
+if [ "$TEST_REGISTRY_CLUSTER_IP" = "" ] ; then
+  fail_test "Cant find test registry cluster ip"
+fi
+if [ "$TEST_REGISTRY_CLUSTER_IP" = "None" ] ; then
+  fail_test "Test regsitry has cluster ip None"
+fi
+
+BUNDLE_PATH="simple/pipeline:latest"
+
+kubectl port-forward -n tekton-resolution-example-registry service/registry 9090:5000 &
+KUBECTL_PORT_FORWARD_PID=$!
+
+# give the port-forward a moment to establish; without this tkn bundle
+# push can emit strange errors like BLOB_UPLOAD_UNKNOWN
+sleep 2
+
+LOCAL_BUNDLE_REF="localhost:9090/${BUNDLE_PATH}"
+export TEST_BUNDLE_REF="${TEST_REGISTRY_CLUSTER_IP}:5000/${BUNDLE_PATH}"
+
+tkn bundle push -f ./test/bundle_registry/pipeline.yaml "$LOCAL_BUNDLE_REF"
+if [ $? -ne 0 ] ; then
+  kill -15 ${KUBECTL_PORT_FORWARD_PID}
+  fail_test "Failed to push bundle to test registry"
+fi
+
+kill -15 ${KUBECTL_PORT_FORWARD_PID}
+
+
+######### Run the Tests ########
 
 header "Running e2e tests"
 # by default runs `go test -tags=e2e`
--- test/pipeline_bundles_test/pipeline_bundles_test.go
+++ test/pipeline_bundles_test/pipeline_bundles_test.go
@@ -26,10 +26,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/tektoncd/resolution/test/helpers"
-	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
-	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
@@ -53,9 +50,18 @@ const waitInterval = time.Second
 // The timeout is high because the CI/CD cluster can be slow.
 const waitTimeout = 2 * time.Minute
 
+// testBundleRefEnvVar is the name of an environment variable that's used to
+// pass the url of a bundle that can be pulled during the e2e test.
+const testBundleRefEnvVar = "TEST_BUNDLE_REF"
+
 // TestPipelineBundle executes a PipelineRun that relies on a Pipeline
 // from a Bundle stored in a registry.
 func TestPipelineBundle(t *testing.T) {
+	bundleRef := os.Getenv(testBundleRefEnvVar)
+	if bundleRef == "" {
+		t.Fatalf("test requires a bundle be made available via environment variable %q", testBundleRefEnvVar)
+	}
+
 	ctx := context.Background()
 	configPath := knativetest.Flags.Kubeconfig
 	clusterName := knativetest.Flags.Cluster
@@ -92,23 +98,6 @@ func TestPipelineBundle(t *testing.T) {
 	}, metav1.CreateOptions{})
 	if err != nil {
 		t.Fatalf("Failed to create namespace %s for tests: %s", testNamespace, err)
-	}
-
-	serviceIP, err := deployRegistry(ctx, kubeClient)
-	if err != nil {
-		t.Fatalf("error deploying registry: %v", err)
-	}
-
-	bundleRef := fmt.Sprintf("%s:5000/simple/pipeline:latest", serviceIP)
-
-	pipelineYAML, err := os.ReadFile("./pipeline.yaml")
-	if err != nil {
-		t.Fatalf("unable to read pipeline yaml fixture: %v", err)
-	}
-
-	err = publishTestBundle(ctx, kubeClient, string(pipelineYAML), bundleRef)
-	if err != nil {
-		t.Fatalf("error publishing bundle: %v", err)
 	}
 
 	pipelineRunYAML, err := os.ReadFile("./pipelinerun.yaml")
@@ -196,85 +185,4 @@ func getPipelineRunConditions(pr *unstructured.Unstructured) ([]map[string]strin
 		ret = append(ret, conditionMap)
 	}
 	return ret, nil
-}
-
-// publishTestBundle uses a pod running tkn to push a YAML file as a
-// bundle to the registry identified by bundleRef.
-func publishTestBundle(ctx context.Context, kubeClient kubernetes.Interface, bundleContent, bundleRef string) error {
-	pod := &v1.Pod{}
-	if err := helpers.ReadFileToObject("./pod-tkn-bundle-push.yaml", pod); err != nil {
-		return fmt.Errorf("error reading pod to publish bundle: %w", err)
-	}
-	pod.ObjectMeta.Namespace = testNamespace
-	pod.ObjectMeta.Annotations["pipeline_yaml"] = bundleContent
-	pod.ObjectMeta.Annotations["bundle_ref"] = bundleRef
-	_, err := kubeClient.CoreV1().Pods(testNamespace).Create(ctx, pod, metav1.CreateOptions{})
-	if err != nil {
-		return fmt.Errorf("error creating pod to publish bundle: %w", err)
-	}
-	err = wait.PollImmediate(waitInterval, waitTimeout, func() (bool, error) {
-		p, err := kubeClient.CoreV1().Pods(testNamespace).Get(ctx, pod.ObjectMeta.Name, metav1.GetOptions{})
-		if err != nil {
-			return false, err
-		}
-		switch p.Status.Phase {
-		case corev1.PodSucceeded:
-			return true, nil
-		case corev1.PodFailed:
-			return false, fmt.Errorf("error publishing bundle. pod status: %#v", p.Status)
-		default:
-			return false, nil
-		}
-	})
-	return err
-}
-
-// deployRegistry submits a registry deployment and service to
-// kubernetes and then waits for them to come up and be ready. The
-// clusterIP of the service is returned on success.
-func deployRegistry(ctx context.Context, kubeClient kubernetes.Interface) (string, error) {
-	deployment := &appsv1.Deployment{}
-	if err := helpers.ReadFileToObject("./registry-deployment.yaml", deployment); err != nil {
-		return "", fmt.Errorf("error reading registry deployment: %w", err)
-	}
-
-	service := &corev1.Service{}
-	if err := helpers.ReadFileToObject("./registry-service.yaml", service); err != nil {
-		return "", fmt.Errorf("error reading registry service: %w", err)
-	}
-
-	_, err := kubeClient.AppsV1().Deployments(testNamespace).Create(ctx, deployment, metav1.CreateOptions{})
-	if err != nil {
-		return "", fmt.Errorf("error creating registry deployment: %w", err)
-	}
-
-	_, err = kubeClient.CoreV1().Services(testNamespace).Create(ctx, service, metav1.CreateOptions{})
-	if err != nil {
-		return "", fmt.Errorf("error creating registry service: %w", err)
-	}
-
-	err = wait.PollImmediate(waitInterval, waitTimeout, func() (bool, error) {
-		d, err := kubeClient.AppsV1().Deployments(testNamespace).Get(ctx, deployment.Name, metav1.GetOptions{})
-		if err != nil {
-			return false, err
-		}
-		s, err := kubeClient.CoreV1().Services(testNamespace).Get(ctx, service.Name, metav1.GetOptions{})
-		if err != nil {
-			return false, err
-		}
-		if d.Status.ReadyReplicas > 0 && s.Spec.ClusterIP != "" && s.Spec.ClusterIP != corev1.ClusterIPNone {
-			return true, nil
-		}
-		return false, nil
-	})
-	if err != nil {
-		return "", fmt.Errorf("error waiting for deployment and service: %w", err)
-	}
-
-	s, err := kubeClient.CoreV1().Services(testNamespace).Get(ctx, service.Name, metav1.GetOptions{})
-	if err != nil {
-		return "", fmt.Errorf("error getting service: %w", err)
-	}
-
-	return s.Spec.ClusterIP, nil
 }
