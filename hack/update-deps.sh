8458beb58ba34af443f21558ffcddb4e74232da9
Scott <sbws@google.com>
2022-04-07 12:16:47 +0000

    Dependabot notified that zap needs to be updated
    This commit updates the zap dependency and fixes CI which seemed to fail
    due to go mod tidy not including the -compat=1.17 flag.

--- hack/update-deps.sh
+++ hack/update-deps.sh
@@ -22,4 +22,90 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 
 source "$REPO_ROOT/vendor/knative.dev/hack/library.sh"
 
+# This is a copy/paste of knative's library.sh function. We've added
+# this to force the `go mod tidy` command to use the -compat=1.17 flag.
+# Without this the call to go_update_deps doesn't seem to do anything
+# but print an error.
+#
+# Update go deps.
+# Parameters (parsed as flags):
+#   "--upgrade", bool, do upgrade.
+#   "--release <release-version>" used with upgrade. The release version to upgrade
+#                         Knative components. ex: --release v0.18. Defaults to
+#                         "main".
+#   "--module-release <module-version>" used to define a different go module tag
+#                         for a release. ex: --release v1.0 --module-release v0.27
+# Additional dependencies can be included in the upgrade by providing them in a
+# global env var: FLOATING_DEPS
+# --upgrade will set GOPROXY to direct unless it is already set.
+function go_update_deps() {
+  cd "${REPO_ROOT_DIR}" || return 1
+
+  export GO111MODULE=on
+  export GOFLAGS=""
+  export GONOSUMDB="${GONOSUMDB:-},knative.dev/*"
+  export GONOPROXY="${GONOPROXY:-},knative.dev/*"
+
+  echo "=== Update Deps for Golang"
+
+  local UPGRADE=0
+  local RELEASE="v9000.1" # release v9000 is so far in the future, it will always pick the default branch.
+  local RELEASE_MODULE=""
+  local DOMAIN="knative.dev"
+  while [[ $# -ne 0 ]]; do
+    parameter=$1
+    case ${parameter} in
+      --upgrade) UPGRADE=1 ;;
+      --release) shift; RELEASE="$1" ;;
+      --module-release) shift; RELEASE_MODULE="$1" ;;
+      --domain) shift; DOMAIN="$1" ;;
+      *) abort "unknown option ${parameter}" ;;
+    esac
+    shift
+  done
+
+  if [[ $UPGRADE == 1 ]]; then
+    local buoyArgs=(--release ${RELEASE}  --domain ${DOMAIN})
+    if [ -n "$RELEASE_MODULE" ]; then
+      group "Upgrading for release ${RELEASE} to release module version ${RELEASE_MODULE}"
+      buoyArgs+=(--module-release ${RELEASE_MODULE})
+    else
+      group "Upgrading to release ${RELEASE}"
+    fi
+    FLOATING_DEPS+=( $(run_go_tool knative.dev/test-infra/buoy buoy float ${REPO_ROOT_DIR}/go.mod "${buoyArgs[@]}") )
+    if [[ ${#FLOATING_DEPS[@]} > 0 ]]; then
+      echo "Floating deps to ${FLOATING_DEPS[@]}"
+      go get -d ${FLOATING_DEPS[@]}
+    else
+      echo "Nothing to upgrade."
+    fi
+  fi
+
+  group "Go mod tidy and vendor"
+
+  # Prune modules.
+  local orig_pipefail_opt=$(shopt -p -o pipefail)
+  set -o pipefail
+  go mod tidy -compat=1.17 2>&1 | grep -v "ignoring symlink" || true
+  go mod vendor 2>&1 |  grep -v "ignoring symlink" || true
+  eval "$orig_pipefail_opt"
+
+  group "Removing unwanted vendor files"
+
+  # Remove unwanted vendor files
+  find vendor/ \( -name "OWNERS" \
+    -o -name "OWNERS_ALIASES" \
+    -o -name "BUILD" \
+    -o -name "BUILD.bazel" \
+    -o -name "*_test.go" \) -exec rm -f {} +
+
+  export GOFLAGS=-mod=vendor
+
+  group "Updating licenses"
+  update_licenses third_party/VENDOR-LICENSE "./..."
+
+  group "Removing broken symlinks"
+  remove_broken_symlinks ./vendor
+}
+
 go_update_deps "$@"
